34bd8a1326bca53642a1cf1cdc996df13fe6ab6d
eaalberton <emerson_kgt@hotmail.com>
2023-07-26 07:03:57 -0300

    auth implementation

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,46 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CheckContestationDTO } from '../model/CheckContestation.js';
+import { HttpUtils } from '../http';
+import { environment } from 'src/environments/environment';
+import { CredentialsDto } from '../model/CredentialsDto.js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly URL_API = `${environment.API}`;
+
+  isLogged = new EventEmitter<boolean>();
+
+  constructor(
+    private http:HttpClient, 
+    private utils: HttpUtils
+    ) { }
+
+    getAuthToken(): string | null {
+      return window.localStorage.getItem("auth_token");
+    }
+  
+    setAuthToken(token: string | null): void {
+      if (token !== null) {
+        window.localStorage.setItem("auth_token", token);
+        this.isLogged.emit(true);
+      } else {
+        window.localStorage.removeItem("auth_token");
+        this.isLogged.emit(false);
+      }
+    }
+
+  login(credentialsDto:CredentialsDto): Observable<any> {
+    const url = `${this.URL_API}login`;
+    return this.http.post<CheckContestationDTO>(url, credentialsDto);
+  }
+
+  register(credentialsDto:CredentialsDto): Observable<any> {
+    const url = `${this.URL_API}register`;
+    return this.http.post<CheckContestationDTO>(url, credentialsDto);
+  }
+}
